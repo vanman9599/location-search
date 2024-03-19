9e36733f8425e794854d1a796488e877173b75d7
unknown <vanjordan46@hotmail.com>
2024-03-19 15:33:45 -0700

    map search functionality complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { Place } from "./api/Place";
+import Map from "./components/Map";
+import LocationSearch from "./components/LocationSearch";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [place, setPlace] = useState<Place | null>(null)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-screen w-screen grid grid-cols-12">
+      
+      <div className="col-span-3 p-2"><LocationSearch onPlaceClick={(p)=> setPlace(p)} /></div>
+      <div className="col-span-9"><Map place={place} /></div>
+    </div>
   )
 }
 
---
+++ src/components/LocationSearch.tsx
@@ -0,0 +1,34 @@
+import type { Place } from "../api/Place";
+import { useState, Fragment} from "react";
+import { search } from '../api/Search'
+
+interface LocationSearchProps{
+    onPlaceClick: (place: Place)=> void;
+}
+
+export default function LocationSearch({onPlaceClick}: LocationSearchProps){
+    const[places, setPlaces] = useState<Place[]>([]);
+    const [term,setTerm] = useState('')
+
+ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+     const results = await search(term)
+     setPlaces(results);
+ }
+
+    return (<div>
+        <form onSubmit={handleSubmit}>
+            <label className="font-bold" htmlFor="term"> Search</label>
+            <input id="term" className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full" value={term} onChange={(e=> setTerm(e.target.value))} />
+        </form>
+        <h1 className="font-bold mt-6">Found Locations</h1>
+        <div className="grid grid-cols-[1fr-40px] gap-2 mt-2 items-center">
+            {places.map(place=>{
+                return <Fragment key={place.id}>
+                    <p>{place.name}</p>
+                    <button className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded" onClick={() => onPlaceClick(place)}>Go</button>
+                <div className="border-b w-full col-span-2"/>                </Fragment>
+            })}
+        </div>
+    </div>)
+}
